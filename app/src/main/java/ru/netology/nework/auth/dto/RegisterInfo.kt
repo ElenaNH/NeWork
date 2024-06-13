f914eb13c41469134414dd0c244bbda1cfa6c191
Elena Nefedova <elenanefedova@gmail.com>
2024-06-13 09:10:44 +0300

    RESTORED Authentification and some more dependencies

--- app/src/main/java/ru/netology/nework/auth/dto/RegisterInfo.kt
+++
@@ -1,9 +0,0 @@
-package ru.netology.nework.auth.dto
-
-data class RegisterInfo(
-    val username: String = "",
-    val login: String = "",
-    val password: String = "",
-    val password2: String = "",
-    val avatar: String? = null,
-)
--- app/src/main/java/ru/netology/nework/auth/dto/UserResponse.kt
+++
@@ -1,8 +0,0 @@
-package ru.netology.nework.auth.dto
-
-data class UserResponse (
-    val id: Int,
-    val login: String,
-    val name: String,
-    val avatar: String?
-)
