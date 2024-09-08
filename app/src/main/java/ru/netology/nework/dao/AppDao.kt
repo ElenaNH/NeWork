8d8006162bcf2c3e53d9c3ed11f55b74ec1de148
Elena Nefedova <elenanefedova@gmail.com>
2024-09-08 19:58:45 +0300

    Registration with avatar cured BUT suspend keyword ERROR

--- app/src/main/java/ru/netology/nework/dao/AppDao.kt
+++ app/src/main/java/ru/netology/nework/dao/AppDao.kt
@@ -10,13 +10,21 @@ interface AppDao {
     @Query("SELECT * FROM UserEntity")
     fun getAllUsers(): Flow<List<UserEntity>>
 
-/*    @Query("SELECT * FROM UserEntity")
+    @Query("SELECT count(*) FROM UserEntity")
+    fun countUsers(): Int
+
+    @Query("SELECT COUNT(*)  == 0 FROM UserEntity")
+    //suspend
+    fun isEmpty(): Boolean
+
+    /* // Компилится тольк БЕЗ suspend, хотя в учебном проекте аналогичное было с suspend
+    @Query("SELECT * FROM UserEntity")
     suspend fun getAllUsersAlternate(): List<UserEntity>*/
 
     /*@Query("SELECT * FROM UserEntity WHERE id = :id")
     suspend fun getUserById(id:Long): List<UserEntity>*/
 
-    /*@Query("SELECT UserEntity.* FROM UserEntity, AuthEntity WHERE (UserEntity.id = AuthEntity.id) AND AuthEntity.authenicated")
+    /*@Query("SELECT UserEntity.* FROM UserEntity, AuthEntity WHERE (UserEntity.id == AuthEntity.id) AND AuthEntity.authenicated")
     suspend fun getCurrentUser(): List<UserEntity>*/
 
 }
